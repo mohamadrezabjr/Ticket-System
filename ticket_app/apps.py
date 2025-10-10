ee36ecb5959557fffc0c24969d90aec30bf4b46f
mohamadreza <mohamadrezabjr@gmail.com>
2025-10-10 18:04:13 +0330

    Rename apps

--- ticket_app/apps.py
+++ ticket_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class TicketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = 'ticket_app'
