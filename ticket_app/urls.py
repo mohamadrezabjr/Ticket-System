d4c761220b83132465db3a5faa203dbe7ecb817d
mohamadreza <mohamadrezabjr@gmail.com>
2025-10-18 21:47:06 +0330

    change categories/ to ticket-categories/ in ticket_app urls

--- ticket_app/urls.py
+++ ticket_app/urls.py
@@ -7,6 +7,6 @@ router.register('tickets', views.TicketViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('tickets/<int:ticket_id>/messages/', views.MessagesListCreateView.as_view(), name='messages'),
-    path('categories/', views.CategoryListCreateAPIView.as_view(), name = 'category-list-create'),
-    path('categories/<int:category_id>/',views.CategoryDetailAPIView.as_view(), name = 'category-deatail')
+    path('ticket-categories/', views.CategoryListCreateAPIView.as_view(), name = 'category-list-create'),
+    path('ticket-categories/<int:category_id>/',views.CategoryDetailAPIView.as_view(), name = 'category-deatail')
 ]
