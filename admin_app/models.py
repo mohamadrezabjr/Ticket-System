5855e6f76dae443ce0f653d00bd96e3716ac0816
mohamadreza <mohamadrezabjr@gmail.com>
2025-10-19 17:50:25 +0330

    Change NotificationCategory options.

--- admin_app/models.py
+++ admin_app/models.py
@@ -4,19 +4,18 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class NotificationCategeory(models.TextChoices):
-    info = 'اطلاع رسانی'
-    warning = 'هشدار'
-    promo = 'تبلیغات'
-
+    NEW_MESSAGE = 'N', 'پیام جدید از پشتیبانی'
+    TICKET_CLOSE = 'C', 'بسته شدن تیکت'
+    SYSTEM = 'S', 'اعلان سیستمی'
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'notifications')
-    title = models.CharField(max_length = 255)
-    category = models.CharField(max_length = 30, choices = NotificationCategeory.choices, default = NotificationCategeory.info)
+    title = models.CharField(max_length = 255, null = True, blank = True)
+    category = models.CharField(max_length = 30, choices = NotificationCategeory.choices, default = NotificationCategeory.NEW_MESSAGE)
     message = models.TextField()
     is_read = models.BooleanField(default = False)
     created_at = models.DateTimeField(auto_now_add = True)
     
     def __str__(self) -> str:
-        return self.user
+        return f'{self.get_category_display()} برای {self.user.phone}'
     
