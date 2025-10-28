cb3132c77ec7ecdc07e4a998e1dc2bab292f1a03
mohamadreza <mohamadrezabjr@gmail.com>
2025-10-29 00:52:52 +0330

    Add redis_url to celery

--- ticket_system/celery.py
+++ ticket_system/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticket_system.settings')
 
-app = Celery('ticket_system')
+app = Celery('ticket_system', broker=os.environ.get('REDIS_URL'))
 
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
